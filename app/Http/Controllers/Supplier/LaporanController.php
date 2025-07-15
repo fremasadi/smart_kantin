c4fa0e6a19305a43baac0111decd9fd2c0b2f013
fremasadi <fremasadii@gmail.com>
2025-07-15 23:21:40 +0700

    feat

--- app/Http/Controllers/Supplier/LaporanController.php
+++ app/Http/Controllers/Supplier/LaporanController.php
@@ -3,44 +3,35 @@
 namespace App\Http\Controllers\Supplier;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
 use App\Models\OrderItem;
 use App\Exports\SupplierOrderExport;
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Http\Request;
 
-
 class LaporanController extends Controller
 {
     public function index(Request $request)
-{
-    $supplierId = Auth::id();
+    {
+        $query = OrderItem::with(['order', 'product']);
 
-    $query = OrderItem::with(['order', 'product'])
-        ->whereHas('product', function ($q) use ($supplierId) {
-            $q->where('supplier_id', $supplierId);
-        });
-
-    // Filter berdasarkan tanggal
-    if ($request->filter === 'hari_ini') {
-        $query->whereDate('created_at', now()->toDateString());
-    } elseif ($request->filter === 'kemarin') {
-        $query->whereDate('created_at', now()->subDay()->toDateString());
-    } elseif ($request->filter === 'custom') {
-        if ($request->filled(['dari', 'sampai'])) {
-            $query->whereBetween('created_at', [$request->dari, $request->sampai]);
+        // Filter berdasarkan tanggal
+        if ($request->filter === 'hari_ini') {
+            $query->whereDate('created_at', now()->toDateString());
+        } elseif ($request->filter === 'kemarin') {
+            $query->whereDate('created_at', now()->subDay()->toDateString());
+        } elseif ($request->filter === 'custom') {
+            if ($request->filled(['dari', 'sampai'])) {
+                $query->whereBetween('created_at', [$request->dari, $request->sampai]);
+            }
         }
+
+        $orderItems = $query->orderByDesc('created_at')->get();
+
+        return view('supplier.laporan.index', compact('orderItems'));
     }
 
-    $orderItems = $query->orderByDesc('created_at')->get();
-
-    return view('supplier.laporan.index', compact('orderItems'));
-}
-
-
     public function export()
-{
-    return Excel::download(new SupplierOrderExport, 'laporan_penjualan.xlsx');
-}
-
+    {
+        return Excel::download(new SupplierOrderExport, 'laporan_penjualan.xlsx');
+    }
 }
